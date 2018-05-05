6bd644c23a9ce138d4b8622ff5431645acb5f8fd
crimson-med <mederic.burlet@gmail.com>
2018-05-05 20:40:03 +0800

    Adding the responsive size possibility

--- app.js
+++ app.js
@@ -25,18 +25,40 @@ function castColor(color){
   }
   return toCast;
 };
-
+function castSize(progressSize, buffer = 0){
+  const maxSize = process.stdout.columns;
+  let toCast = maxSize/2;
+  if (progressSize === "xsmall"){
+    toCast = (maxSize/4).toFixed(0);
+  }else if (progressSize === "small"){
+    toCast = (maxSize/3).toFixed(0);
+  }else if (progressSize === "medium"){
+    toCast = (maxSize/2).toFixed(0);
+  }else if (progressSize === "large"){
+    toCast = ((maxSize/3)*2).toFixed(0);
+  }else if (progressSize === "xlarge"){
+    toCast = maxSize;
+  }
+  if (toCast + buffer >= maxSize){
+    toCast = toCast - buffer -1;
+  }
+  return toCast;
+}
 module.exports = {
-  renderProgressBar: async function (currentValue, totalValue, progressColor = "green", backgroundColor = "yellow", progressSymbol = "=", backgroundSymbol = "-", renderContainer){
+  renderProgressBar: async function (currentValue, totalValue, progressColor = "green", backgroundColor = "yellow", progressSymbol = "=", backgroundSymbol = "-", renderContainer, progressSize){
     let foreProgress = castColor(progressColor);
     let backProgress = castColor(backgroundColor);
-
+    let lengthBuffer = currentValue.toString().length + totalValue.toString().length + 3;
+    if (renderContainer === true){
+      lengthBuffer = lengthBuffer + 2;
+    }
+    let lengthProgress = castSize(progressSize, lengthBuffer);
     process.stdout.clearLine();  // clear current text
     process.stdout.cursorTo(0);  // move cursor to beginning of line
-    const percent = ((currentValue)/totalValue*50).toFixed(2) ;
+    const percent = ((currentValue)/totalValue*lengthProgress).toFixed(2) ;
     let progressFirst = "";
     let progressLast = "";
-    for (let j = 0; j < 50; j++) {
+    for (let j = 0; j < lengthProgress; j++) {
       if (j <= percent) {
         progressFirst = progressFirst + progressSymbol;
       }else{
